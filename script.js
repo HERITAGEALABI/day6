b67d7107c122590ee776bdf64e21b84d5bba3588
heritagealabi@gmail.com <heritagealabi@gmail.com>
2020-05-07 00:17:10 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+window.onload = function() {
+    document.getElementById('menulink').onclick = function() {
+        var menu = document.getElementById('menu');
+        if(menu.className != 'shownmenu') {
+            menu.className = 'shownmenu';
+        }
+        else{
+            menu.className = 'hiddenmenu';
+        }
+    }
+}
+//retrive saved note
+function check_web_storage_support() {
+    if(typeof(Storage) !== "undefined") {
+        return(true);
+    }
+    else {
+        alert("Web storage unsupported!");
+        return(false);
+    }
+}
+//implement functionality of save button
+function display_saved_note() {
+    if(check_web_storage_support() == true) {
+        result = localStorage.getItem('note');
+    }
+    if(result === null) {
+        result = "No note saved";
+    }
+    document.getElementById('area').value = result;
+}
+//
+function clear() {
+    document.getElementById('area').value = "";
+}
